40090aa1fa78255e258ce3ebb441927f7e7e9359
Adwait Kulkarni <adwait.kul.2018@gmail.com>
2022-03-17 22:11:02 -0700

    Create index.js

---
+++ Steps_To_Reach_Zero/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let stepCount = 0;
+    while (num != 0) {
+        if (num % 2 == 0) {
+            num = num / 2;
+            stepCount++;
+        } else {
+            num = num - 1;
+            stepCount++;
+        }
+    }
+    return stepCount;
+};
